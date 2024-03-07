54a47c8da9a4c5dec702897b4ec54df64e093808
kaorat <133619456+kaorat@users.noreply.github.com>
2024-03-08 00:27:24 +0700

    BOSS BOSSMANAGER STATMANAGER v.1

---
+++ src/GameEntity/Boss/BaseBoss.java
@@ -0,0 +1,32 @@
+package GameEntity.Boss;
+
+import GameEntity.GameObject;
+
+public abstract class BaseBoss extends GameObject {
+    protected int hp;
+    protected int fireRate;
+    protected int bulletSpeed;
+    protected int bulletLength;
+
+//    boss should drop sth????
+
+    public BaseBoss(int hp,int fireRate,int bulletSpeed,int bulletLength){
+        this.hp = hp;
+        this.fireRate = fireRate;
+        this.bulletSpeed = bulletSpeed;
+        this.bulletLength = bulletLength;
+    }
+//    to set firing pattern for each boss
+    public abstract void StartFiring();
+    public abstract void firing();
+
+    @Override
+    public void onUpdate() {
+
+    }
+
+    @Override
+    public void draw(GraphicsContext gc) {
+
+    }
+}
---
+++ src/GameEntity/Boss/Boss1.java
@@ -0,0 +1,22 @@
+package GameEntity.Boss;
+
+public class Boss1 extends BaseBoss{
+    public Boss1(int hp, int fireRate, int bulletSpeed, int bulletLength) {
+        super(hp, fireRate, bulletSpeed, bulletLength);
+    }
+
+    @Override
+    public void StartFiring() {
+
+    }
+
+    @Override
+    public void firing() {
+
+    }
+
+    @Override
+    public void draw(GraphicsContext gc) {
+
+    }
+}

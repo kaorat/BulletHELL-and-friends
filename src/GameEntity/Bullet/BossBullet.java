8ad477b6546f1065fc056ab2a94e797b5ca075bc
Gannub <gannubvv@gmail.com>
2024-03-08 21:12:28 +0700

    bullet manager v1

---
+++ src/GameEntity/Bullet/BossBullet.java
@@ -0,0 +1,11 @@
+package GameEntity.Bullet;
+
+import GameEntity.GameObject;
+
+public class BossBullet extends BaseBullet{
+
+    public BossBullet(double damage, GameObject owner) {
+        super(damage, owner);
+    }
+
+}
